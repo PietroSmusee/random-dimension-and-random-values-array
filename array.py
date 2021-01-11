ea9123b3467265afbde9c9a94e3072a3a84c9f0e
PietroSmusee <77297333+PietroSmusee@users.noreply.github.com>
2021-01-11 20:23:28 +0100

    Create array.py

---
+++ array.py
@@ -0,0 +1,13 @@
+from random import *
+
+for n in range(1,20):
+   
+    array = [n]*random.randint(1,20)
+   
+    for i in range(len(array)):
+       
+      array[i]=random.randint(1,20)  
+
+print(array) 
+
+###Riccardo Di Palma
